4b9107229e3f52634d0af2d68ff90a85e7abc9d6
jonathan.rosenbaum@roamdigital.com <72714975+jonathanrosenbaum@users.noreply.github.com>
2022-09-27 07:04:07 +1000

    fix: updates

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
 const scene = new THREE.Scene();
 
@@ -148,8 +149,177 @@ const airplane = () => {
 
 scene.add(airplane());
 
+//orbit controls
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.maxPolarAngle = 180;
+camera.position.set(0, 0, 50);
+controls.update();
+
+const fragmentShader = `
+uniform sampler2D globeTexture;
+varying vec2 vertexUV;
+varying vec3 vertexNormal;
+
+void main() {
+  float intensity = 1.05 - dot(vertexNormal, vec3(0.0, 0.4, 0.4));
+  vec3 atmosphere = vec3(0.3, 0.6, 1.0) * pow(intensity, 1.5);
+  gl_FragColor = vec4(atmosphere + texture2D(globeTexture, vertexUV).xyz, 1.0);
+}
+`;
+//gl_Position x,y,z position and translations
+//uniform is an import like props in react
+//three js by default passes in some variables into this shader which can be used https://threejs.org/docs/#api/en/renderers/webgl/WebGLProgram
+const vertexShader = `
+varying vec2 vertexUV;
+varying vec3 vertexNormal;
+
+void main() {
+  vertexUV = uv;
+  vertexNormal = normalize(normalMatrix * normal);
+  vec3 newposition = position + position*sin(position.x)*position.z * 0.005*cos(position.y);
+  gl_Position = projectionMatrix * modelViewMatrix * vec4( newposition, 1.0 );
+}
+`;
+
+const fragmentShaderAtmosphere = `
+varying vec3 vertexNormal;
+
+void main() {
+  float intensity = pow(0.4 - dot(vertexNormal, vec3(0, 0, 1.0)), 2.0);
+  gl_FragColor = vec4(0.3, 0.6, 1.0, 0.4) * intensity;
+}
+`;
+
+const vertexShaderAtmosphere = `
+varying vec3 vertexNormal;
+
+void main() {
+  vertexNormal = normalize(normalMatrix * normal);
+  gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+}
+`;
+
+const sea = () => {
+  const container = new THREE.Object3D();
+  const geom = new THREE.CylinderGeometry(24, 24, 32, 50, 1);
+  // rotate the geometry on the x axis
+  geom.applyMatrix4(new THREE.Matrix4().makeRotationX(Math.PI / 2));
+  // create the material
+  const mat = new THREE.ShaderMaterial({
+    vertexShader,
+    fragmentShader,
+    uniforms: {
+      globeTexture: {
+        value: new THREE.TextureLoader().load("./textures/water.jpg"),
+      },
+    },
+  });
+
+  let seaMesh = new THREE.Mesh(geom, mat);
+  seaMesh.name = "sea";
+  container.add(seaMesh);
+  container.position.set(1, -30, 0);
+
+  const atmosphere = new THREE.Mesh(
+    new THREE.SphereGeometry(30, 50, 50),
+    new THREE.ShaderMaterial({
+      vertexShader: vertexShaderAtmosphere,
+      fragmentShader: fragmentShaderAtmosphere,
+      blending: THREE.AdditiveBlending,
+      side: THREE.BackSide,
+    })
+  );
+  atmosphere.scale.set(1.3, 1.3);
+  container.add(atmosphere);
+
+  seaMesh.updateMatrix();
+  return container;
+};
+
+scene.add(sea());
+
+// create moon
+const createMoon = () => {
+  const sphereGeometry = new THREE.SphereGeometry(20, 64, 32);
+  const texture = new THREE.TextureLoader().load("./textures/water.jpg");
+  const sphereMaterial = new THREE.MeshPhongMaterial({
+    map: texture,
+    color: "brown",
+  });
+  const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+  sphere.name = "moon";
+  const torusGeometry = new THREE.TorusGeometry(24, 2, 16, 100);
+  const torusMaterial = new THREE.MeshPhongMaterial({
+    color: "green",
+    shading: THREE.FlatShading,
+  });
+  const torusMaterialBasic = new THREE.MeshBasicMaterial({
+    color: "green",
+    shading: THREE.FlatShading,
+  });
+  const torusMesh = new THREE.Mesh(torusGeometry, torusMaterial);
+  const torusMeshBasic = new THREE.Mesh(torusGeometry, torusMaterialBasic);
+  torusMeshBasic.position.x = 0;
+  torusMeshBasic.position.z = -5;
+  sphere.add(torusMesh, torusMeshBasic);
+  scene.add(sphere);
+  sphere.rotation.x = (3 * Math.PI) / 2;
+  sphere.position.set(150, 250, -400);
+};
+createMoon();
+
+const vertices = [];
+
+for (let i = 0; i < 10000; i++) {
+  const x = THREE.MathUtils.randFloatSpread(2000);
+  const y = THREE.MathUtils.randFloatSpread(2000);
+  const z = THREE.MathUtils.randFloatSpread(2000);
+
+  vertices.push(x, y, z);
+}
+
+const geometry = new THREE.BufferGeometry();
+geometry.setAttribute(
+  "position",
+  new THREE.Float32BufferAttribute(vertices, 3)
+);
+
+const material = new THREE.PointsMaterial({ color: 0x888888 });
+
+const points = new THREE.Points(geometry, material);
+
+scene.add(points);
+
+let airplanePos = { x: 0, y: 0 };
+const handleMouseMove = (event) => {
+  const x = -1 + (2 * event.clientX) / window.innerWidth;
+  const y = -1 + 2 * (1 - event.clientY / window.innerHeight);
+
+  //normalise plane movements
+  const normalise = (number) => {
+    let clampedNumber = THREE.MathUtils.clamp(number, -0.4, 0.4);
+    return clampedNumber * 8;
+  };
+
+  airplanePos = { x: normalise(x), y: normalise(y) };
+  // i need to clamp the upper and lower limits and increase multiplier in airplanePos
+  console.log("airplanePos", airplanePos);
+};
+
+window.addEventListener("mousemove", handleMouseMove);
+
+function updatePlane() {
+  // update the airplane's position
+  const airplane = scene.getObjectByName("airplane");
+
+  airplane.position.y = airplanePos.y;
+  airplane.position.x = airplanePos.x;
+}
+
 const loop = () => {
   scene.getObjectByName("propeller").rotation.x += 0.3;
+  scene.getObjectByName("sea").rotation.z += 0.003;
+  updatePlane();
 };
 
 function animate() {
